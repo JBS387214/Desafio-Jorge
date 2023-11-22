06da480771334a22b4fe81c6d73685c6b5d8d757
Jorge Batista <134115195+JBS387214@users.noreply.github.com>
2023-11-22 20:21:07 -0300

    Hero

---
+++ index.js
@@ -0,0 +1,27 @@
+// Solicita ao usuário que insira o nome do herói e a quantidade de XP
+var nomeHeroi = prompt("Digite o nome do herói: ");
+var xpHeroi = parseInt(prompt("Digite a quantidade de experiência (XP) do herói: "));
+
+// Utiliza estrutura de decisão para determinar o nível do herói
+var nivel;
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 6001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Exibe a mensagem com o nome do herói e seu nível
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
